4f74818e79d26f9b5a76d9319a7780f518176a97
SM Sohag <sohagsheik32@gmail.com>
2023-05-18 22:58:16 +0600

    offers content data get and error fix

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 const express = require('express');
+const app = express();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const app = express();
-const port = process.env.PORT || 5050;
+
+const port = process.env.PORT || 3000
 
 dotenv.config();
 
 // middleware
 
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 
 // initial server route
 app.get('/', (req,res)=>{
     res.send('toyWire server is running!')
 })
-
 // mongo db uri
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.d2ul6pd.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -33,18 +33,28 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
    
-    await client.connect();
+    // await client.connect();
    
+    const offerCollection = client.db('toyWireDB').collection('offers');
+
+    app.get('/offers', async(req, res)=>{
+        
+        const offersData = await offerCollection.find().toArray()
+        res.send(offersData)
+    })
+    
+    
+    
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    
-  }
+      
+}
 }
 run().catch(console.dir);
 
-
 app.listen(port, ()=>{
-    console.log(`toyWire server is running on port ${port}`);
+  console.log(`server is running `);
 })
+
 
