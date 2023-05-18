dd40fa5cadda5c5348469c4aab21da6f4d3d8aa9
SM Sohag <sohagsheik32@gmail.com>
2023-05-18 21:08:59 +0600

    mongodb database connect and implemented

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const app = express();
@@ -11,15 +12,36 @@ dotenv.config();
 app.use(cors);
 app.use(express.json());
 
-
-
-
 // initial server route
-
 app.get('/', (req,res)=>{
     res.send('toyWire server is running!')
 })
 
+// mongo db uri
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.d2ul6pd.mongodb.net/?retryWrites=true&w=majority`;
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+async function run() {
+  try {
+   
+    await client.connect();
+   
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    
+  }
+}
+run().catch(console.dir);
 
 
 app.listen(port, ()=>{
