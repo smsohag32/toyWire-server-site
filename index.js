c9cd228bee9f719265a120c49c03e07343f75b0e
SM Sohag <sohagsheik32@gmail.com>
2023-06-04 19:38:14 +0600

    db connect

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const dotenv = require('dotenv');
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 4200
 dotenv.config();
 
 
@@ -19,7 +19,7 @@ app.use(express.json())
 
 
 // mongo db uri
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xxly7bw.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.d2ul6pd.mongodb.net/?retryWrites=true&w=majority`;
 
 
 const client = new MongoClient(uri, {
